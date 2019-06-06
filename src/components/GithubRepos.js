987073fe48e1fa46fa2b504896621c469482c044
“Naciri <93naciri@gmail.com>
2019-06-06 20:44:25 +0100

    Listing Repos from github created 30 days ago

--- src/components/GithubRepos.js
+++ src/components/GithubRepos.js
@@ -1,28 +1,72 @@
 import React from 'react';
 
 class GithubRepos extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      repos: [],
+      isLoaded: false,
+      page: 1
+    }
+  }
+
+  componentDidMount = () =>{
+    var date = new Date();
+    date.setDate(date.getDate() - 30);
+    var dateString = date.toISOString().split('T')[0];
+    
+    fetch ('https://api.github.com/search/repositories?q=created:>'+dateString+'&sort=stars&order=desc')
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        repos: json.items,
+        total_count: json.total_count
+      });
+    })
+    window.scrollTo(0, 0)
+  }
 
   render(){
-  return(
-    <div className="container">
-      <div className="card mb-3" >
-        <div className="row no-gutters">
-          <div className="col-md-2">
-            <img src="..." className="card-img" alt='...'/>
+    var { isLoaded, repos, total_count } = this.state;
+    if (!isLoaded){
+      return <h1> Loading Data ...</h1>;
+    }else{
+      return(
+        <div className="container">
+          <h1>Total Github Repos: {total_count} </h1>
+          {repos.map(item =>(
+          <div className="card mb-3" key={item.id}>
+            <div className="row no-gutters">
+              <div className="col-md-2">
+                <img src={item.owner.avatar_url} className="card-img" alt={item.owner.login}/>
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">{item.full_name}</h5>
+                  <p className="card-text">{ item.description }</p>
+                  <p className="card-text">stars :<small className="text-muted">{ item.stargazers_count}</small> 
+                  | Issues :<small>{item.open_issues}</small> | Submitted 30 days ago by {item.owner.login}
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">Repo Name</h5>
-              <p className="card-text">Repos Description</p>
-              <p className="card-text">stars :<small className="text-muted"> 11</small> 
-              | Issues :<small>0</small> | Submitted 30 days ago by RecaN
-              </p>
+          ))}
+          <div className="row">
+            <div className="col-sm">
+              <button className="btn btn-dark" > Previous page </button>
+            </div>
+            <div className="col-sm">
+            </div>
+            <div className="col-sm">
+              <button className="btn btn-dark" > Next page </button>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  )}
+        )
+    }
+  }
 }
 
 export default GithubRepos;
